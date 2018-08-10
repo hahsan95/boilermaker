3b9e9e8c8b13af69397ccf4b2a867ecce2c66500
Homum Ahsan <hahsan95@gmail.com>
2018-08-10 12:07:41 -0500

    got api working

--- server/api/messages.js
+++ server/api/messages.js
@@ -1,13 +1,10 @@
 const router = require('express').Router()
 const {Messages} = require('../db/models')
+module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    let allMessages = await Messages.findAll({
-      where: {
-
-      }
-    })
+    let allMessages = await Messages.findAll()
     res.json(allMessages)
   } catch (err) {
     console.error(err)
